8f11e3f3518c3e0a66cbe92e2fc4c18888573eb5
Ulrik Långström <odalrick@gmail.com>
2012-03-14 23:22:27 +0100

    Some css for the map

--- js/view/z.view.TrivialMapViewModel.js
+++ js/view/z.view.TrivialMapViewModel.js
@@ -1,20 +1,31 @@
 var z = z || {};
 z.view = z.view || {};
 
-z.view.TrivialMapViewModel = function(trivialMap){
+z.view.TrivialMapViewModel = function ( trivialMap ) {
     this.mapData = ko.observableArray();
-    this.load = function(){
-        for(var y = 0; y <= 20; ++y){
+    this.load = function () {
+        for ( var y = 0; y <= 20; ++y ) {
             var row = ko.observableArray();
-            this.mapData.push(row);
-            for(var x = 0; x <= 20; ++x){
-                var tileVM = new z.view.TrivialTileViewModel(trivialMap.getTile(x, y));
-                row.push(tileVM);
+            row.rowClass = 'e' + y % 2 + 'mod2';
+            this.mapData.push( row );
+            for ( var x = 0; x <= 20; ++x ) {
+                var tileVM = new z.view.TrivialTileViewModel( trivialMap.getTile( x, y ) );
+                tileVM.x( x );
+                tileVM.y( y );
+                row.push( tileVM );
             }
         }
     };
 };
 
-z.view.TrivialTileViewModel = function(tile){
-  this.terrain = ko.observable(tile.terrain);
+z.view.TrivialTileViewModel = function ( tile ) {
+    var self = this;
+    this.terrain = ko.observable( tile.terrain );
+    this.x = ko.observable();
+    this.y = ko.observable();
+    this.terrainClass = ko.computed( function () {
+        var mod2 = self.y() % 2;
+        var emod2 = ' e' + mod2 + 'mod2';
+        return self.terrain() + ' tile' + emod2;
+    } );
 };
