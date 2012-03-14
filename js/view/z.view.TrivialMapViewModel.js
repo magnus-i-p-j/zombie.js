ead56f0f282c567a95005c425fc751291616b482
Magnus Jonneryd <magnus.jonneryd@gmail.com>
2012-03-14 09:05:35 +0100

    Figured out how to bind to a nested array. NOTE, that you have to use push in order to populate a ObservableArray correctly.

--- js/view/z.view.TrivialMapViewModel.js
+++ js/view/z.view.TrivialMapViewModel.js
@@ -3,14 +3,13 @@ z.view = z.view || {};
 
 z.view.TrivialMapViewModel = function(trivialMap){
     this.mapData = ko.observableArray();
-    this.qwerty = "QWERTY";
     this.load = function(){
         for(var y = 0; y <= 20; ++y){
-            this.mapData[y] = ko.observableArray();
-            var row = this.mapData[y];
+            var row = ko.observableArray();
+            this.mapData.push(row);
             for(var x = 0; x <= 20; ++x){
                 var tileVM = new z.view.TrivialTileViewModel(trivialMap.getTile(x, y));
-                row[x] = tileVM;
+                row.push(tileVM);
             }
         }
     };
