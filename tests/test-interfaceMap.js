364f5948cea2cff0508398ee8bb4c9e10bc43b02
Ulrik Långström <odalrick@gmail.com>
2012-03-16 21:20:01 +0100

    Cleanup in aile 3

--- tests/test-interfaceMap.js
+++ tests/test-interfaceMap.js
@@ -1,43 +1,42 @@
 var interfaceMap = function ( cls ) {
-    var t = {};
+    var TestMap = z.engine[cls];
+    var t = {
+        'test has method getTiles': function () {
+            var map = new TestMap();
+
+            assertFunction( map.getTiles );
+        },
+        'test has method getTile': function () {
+            var map = new TestMap();
+
+            assertFunction( map.getTile );
+        },
+
+        'test getTile returns a tile': function () {
+            var map = new TestMap();
+
+            var tile = map.getTile( 0, 0 );
+
+            assertString( tile.terrain );
+        },
+
+        'test getTile updates data': function () {
+            var map = new TestMap();
+            map.feed( [
+                { x: 1, y: 2, terrain: 'grass' }
+            ] );
+
+            var actual = map.getTile( 1, 2 );
+
+            assertEquals( 'grass', actual.terrain );
+        }
+    };
+
     t['test ' + cls + 'exists'] = function () {
-        assertFunction( z.engine[cls] );
-    };
-
-    t['test has method getTiles'] = function () {
-        var map = new z.engine[cls]();
-
-        assertFunction( map.getTiles );
-    };
-
-    t['test has method getTile'] = function () {
-        var map = new z.engine[cls]();
-
-        assertFunction( map.getTile );
-    };
-
-
-    t['test getTile returns a tile'] = function () {
-        var map = new z.engine[cls]();
-
-        var tile = map.getTile( 0, 0 );
-
-        assertString( tile.terrain );
-    };
-
-    t['test getTile updates data'] = function () {
-        var map = new z.engine[cls]();
-        map.feed( [
-            { x: 1, y: 2, terrain: 'grass' }
-        ] );
-
-        var actual = map.getTile( 1, 2 );
-
-        assertEquals( 'grass', actual.terrain );
+        assertFunction( TestMap );
     };
 
     TestCase( 'test ' + cls, t );
 };
 
 interfaceMap( 'TrivialMap' );
-
