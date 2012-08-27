356e252d7867cf514f513be5d26527551458148d
Magnus Jonneryd <magnus.jonneryd@gmail.com>
2012-08-27 16:26:12 +0200

    Merge branch 'master' of F:\Misc\zombie.js with conflicts.

---
+++ js/facet/z.facet.actionFactory.js
@@ -0,0 +1,35 @@
+goog.provide('z.facet.actionFactory');
+
+z.facet.ActionFactory = function () {
+  this._actionCache = new {};
+};
+
+z.facet.ActionFactory.prototype.CreateAction = function(specification){
+  var action;
+  if(this._isCached(specification)){
+    action = this._get(specification);
+  }else{
+    this._set(specification);
+    action = this._get(specification);
+  }
+  return action;
+};
+
+z.facet.ActionFactory.prototype._isCached = function(specification){
+  return this._actionCache[specification.category][specification.title];
+};
+
+z.facet.ActionFactory.prototype._get = function(specification){
+  return this._actionCache[specification.category][specification.title];
+};
+
+z.facet.ActionFactory.prototype._set = function(specification){
+  var action;
+  if(specification.category === "improvement"){
+    //Do some DI if more parameters are needed, but this will do for now.
+    action = new z.client.actions.ImprovementAction(specification);
+  }
+  if(action){
+    this._actionCache[specification.category][specification.title] = action;
+  }
+};
