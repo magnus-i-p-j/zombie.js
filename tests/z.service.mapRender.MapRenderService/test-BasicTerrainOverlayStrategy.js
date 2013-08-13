cb6a52d094cd4d8c84d9c8ef2949ec068091e538
magnus-i-p-j <magnus.jonneryd@gmail.com>
2013-08-13 14:14:33 +0200

    Some refactoring of tests.

--- tests/z.service.mapRender.MapRenderService/test-BasicTerrainOverlayStrategy.js
+++ tests/z.service.mapRender.MapRenderService/test-BasicTerrainOverlayStrategy.js
@@ -2,28 +2,6 @@ TestCase('test BasicTerrainOverlayStrategy', {
   setUp: function () {
     this.terrains = {'water': 0, 'grass': 1, 'hills': 2, 'forest': 3};
     this.strategy = new z.service.mapRender.BasicTerrainOverlayStrategy(this.terrains);
-  },
-  'test Should get all overlaps': function () {
-    var queryString = '?c=grass&nw=water&ne=hills&e=water&se=hills&sw=hills&w=hills';
-    var query = new z.service.mapRender.TerrainOverlayQuery(queryString);
-    var overlaps = this.strategy._getOverlaps(query);
-    assertEquals(2, overlaps.length);
-    assertEquals(['ne'], overlaps[0]);
-    assertEquals(['se','sw','w'], overlaps[1]);
-  },
-  'test Should join overlap that wraps around': function(){
-    var queryString = '?c=grass&nw=hills&ne=hills&e=water&se=hills&sw=hills&w=hills';
-    var query = new z.service.mapRender.TerrainOverlayQuery(queryString);
-    var overlaps = this.strategy._getOverlaps(query);
-    assertEquals(1, overlaps.length);
-    assertEquals(['se','sw','w','nw','ne'], overlaps[0]);
-  },
-  'test Should join overlap that wraps around but only if they are of the same type': function(){
-    var queryString = '?c=grass&nw=forest&ne=hills&e=water&se=hills&sw=hills&w=hills';
-    var query = new z.service.mapRender.TerrainOverlayQuery(queryString);
-    var overlaps = this.strategy._getOverlaps(query);
-    assertEquals(3, overlaps.length);
-    assertEquals([['nw'],['ne'],['se','sw','w']], overlaps);
   }
 
 });
--- tests/z.service.mapRender.MapRenderService/test-TerrainOverlayQuery.js
+++ tests/z.service.mapRender.MapRenderService/test-TerrainOverlayQuery.js
@@ -1,22 +1,52 @@
 TestCase('test TerrainOverlayQuery', {
-  'setUp': function(){
+  'setUp': function () {
     this.terrains = {'water': 0, 'grass': 1, 'hills': 2, 'forest': 3};
   },
   'test Should throw exception when receiving an invalid query': function () {
     var query = "¤#EE##¤";
-    assertException(function(){new z.service.mapRender.TerrainOverlayQuery(this.terrains, query)}, 'InvalidQueryStringException');
+    assertException(function () {
+      new z.service.mapRender.TerrainOverlayQuery(this.terrains, query)
+    }, 'InvalidQueryStringException');
   },
   'test Should throw exception when receiving an unparsable query': function () {
     var query = "?a=b";
-    assertException(function(){new z.service.mapRender.TerrainOverlayQuery(this.terrains,query)}, 'InvalidQueryStringException');
+    assertException(function () {
+      new z.service.mapRender.TerrainOverlayQuery(this.terrains, query)
+    }, 'InvalidQueryStringException');
   },
-  'test Should order overlaps clockwise' : function(){
+  'test Should order overlaps clockwise': function () {
     var queryString = '?c=grass&nw=water&ne=hills&e=water&se=hills&sw=hills&w=hills';
     var query = new z.service.mapRender.TerrainOverlayQuery(this.terrains, queryString);
     var overlaps = query.getOverlaps();
     //assertEquals(2, overlaps.length);
-    assertEquals({terrain:'hills', directions: ['ne']}, overlaps[0]);
-    assertEquals({terrain:'hills', directions: ['se','sw','w']}, overlaps[1]);
+    assertEquals({terrain: 'hills', directions: ['ne']}, overlaps[0]);
+    assertEquals({terrain: 'hills', directions: ['se', 'sw', 'w']}, overlaps[1]);
+  },
+  'test Should get all overlaps': function () {
+    var queryString = '?c=grass&nw=water&ne=hills&e=water&se=hills&sw=hills&w=hills';
+    var query = new z.service.mapRender.TerrainOverlayQuery(this.terrains,queryString);
+    var overlaps = query.getOverlaps();
+    assertEquals(2, overlaps.length);
+    assertEquals(['ne'], overlaps[0]);
+    assertEquals(['se', 'sw', 'w'], overlaps[1]);
+  },
+  'test Should join overlap that wraps around': function () {
+    var queryString = '?c=grass&nw=hills&ne=hills&e=water&se=hills&sw=hills&w=hills';
+    var query = new z.service.mapRender.TerrainOverlayQuery(this.terrains,queryString);
+    var overlaps = query.getOverlaps();
+    assertEquals(1, overlaps.length);
+    assertEquals(['se', 'sw', 'w', 'nw', 'ne'], overlaps[0]);
+  },
+  'test Should join overlap that wraps around but only if they are of the same type': function () {
+    var queryString = '?c=grass&nw=forest&ne=hills&e=water&se=hills&sw=hills&w=hills';
+    var query = new z.service.mapRender.TerrainOverlayQuery(this.terrains,queryString);
+    var overlaps = query.getOverlaps();
+    assertEquals(3, overlaps.length);
+    assertEquals([
+      ['nw'],
+      ['ne'],
+      ['se', 'sw', 'w']
+    ], overlaps);
   }
 });
 
