6c3966e53b480772446a859f661f24abe4ecd1e8
Ulrik Långström <odalrick@gmail.com>
2014-10-30 21:27:32 +0100

    Add drag'n'drop binding to mugd

--- js/mugd/bindings/dragndrop/dragndroptest.js
+++ js/mugd/bindings/dragndrop/dragndroptest.js
@@ -3,14 +3,32 @@ goog.require('mugd.bindings.dragndrop');
 
 dragndroptest.init = function(element) {
 
-  var model = {
-    'list_1': ko.observableArray(["Donald", "Scrooge", "Woody"]),
-    'list_2': ko.observableArray([])
+  var TestModel = function(initialValues, canDrop) {
+    this.canDrop = canDrop;
+    this.data = ko.observableArray(initialValues);
   };
+  TestModel.prototype.doDrop = function(data) {
+    list_2.data.push(data);
+  };
+
+  var list_1 = new TestModel(["Donald", "Scrooge", "Woody"], function() {
+    return true;
+  });
+  var list_2 = new TestModel([], function() {
+    return true;
+  });
+  var list_3 = new TestModel([], function(data) {
+    return data === 'Woody';
+  });
+
+  var model = {
+    'list_1': list_1,
+    'list_2': list_2,
+    'list_3': list_3
+  };
+
   mugd.bindings.dragndrop.addDragNDrop();
   ko.applyBindings(model, element);
-
-  // TODO: drop/drag
 
 };
 
--- js/mugd/bindings/dragndrop/mugd.bindings.dragndrop.js
+++ js/mugd/bindings/dragndrop/mugd.bindings.dragndrop.js
@@ -1,8 +1,8 @@
 goog.provide('mugd.bindings.dragndrop');
 
-mugd.bindings.dragndrop.addDragNDrop = function () {
+mugd.bindings.dragndrop.addDragNDrop = function() {
 
-  var translateElement = function (target, x, y) {
+  var translateElement = function(target, x, y) {
     target.style.webkitTransform =
       target.style.transform =
         'translate(' + x + 'px, ' + y + 'px)';
@@ -10,11 +10,20 @@ mugd.bindings.dragndrop.addDragNDrop = function () {
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
   };
+  var toggleDragTarget = function(isTarget, event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+    if (isTarget) {
+      dropzoneElement.classList.add('mugd-drag-target');
+      draggableElement.classList.add('mugd-can-drop');
+    } else {
+      dropzoneElement.classList.remove('mugd-drag-target');
+      draggableElement.classList.remove('mugd-can-drop');
+    }
+  };
 
   ko.bindingHandlers['dragSource'] = {
-    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-      // This will be called when the binding is first applied to an element
-      // Set up any initial state, event handlers, etc. here
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
       var interactable = interact(element)
         .draggable({
           /**
@@ -24,10 +33,20 @@ mugd.bindings.dragndrop.addDragNDrop = function () {
              *  target: *
              * }} event
            */
-          onmove: function (event) {
-            var target = event.target,
-              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-              y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+          onstart: function(event) {
+            element.classList.add('mugd-dragged');
+          },
+          /**
+           * @param {{
+             *  dx: number,
+             *  dy: number,
+             *  target: *
+             * }} event
+           */
+          onmove: function(event) {
+            var target = event.target
+            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
             translateElement(target, x, y);
           },
           /**
@@ -37,63 +56,59 @@ mugd.bindings.dragndrop.addDragNDrop = function () {
              *  target: *
              * }} event
            */
-          onend: function (event) {
-            var target = event.target;
-            translateElement(target, 0, 0);
+          onend: function(event) {
+            translateElement(event.target, 0, 0);
+            element.classList.remove('mugd-dragged');
           }
         })
         .inertia(true);
 
-        var value = valueAccessor();
-        var valueUnwrapped = ko.unwrap(value);
-        interactable.data = valueUnwrapped;
-    }
-  };
+      var value = valueAccessor();
+      var valueUnwrapped = ko.unwrap(value);
+      interactable.data = valueUnwrapped;
 
-  var toggleDragTarget = function (isTarget, event) {
-    var draggableElement = event.relatedTarget;
-    var dropzoneElement = event.target;
-    if (isTarget) {
-      dropzoneElement.classList.add('drag-target');
-      draggableElement.classList.add('can-drop');
-    } else {
-      dropzoneElement.classList.remove('drag-target');
-      draggableElement.classList.remove('can-drop');
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+        interactable.data = null;
+      });
     }
   };
 
   ko.bindingHandlers['dragTarget'] = {
-    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-      // This will be called when the binding is first applied to an element
-      // Set up any initial state, event handlers, etc. here
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+      var value = valueAccessor();
+      /**
+       * @type {mugd.bindings.dragndrop.IDropZone}
+       */
+      var target = ko.unwrap(value);
+
+      if (!(target.canDrop && target.doDrop)) {
+        console.log(target);
+        throw 'target must implement mugd.bindings.dragndrop.IDropZone';
+      }
+
       var interactable = interact(element)
         .dropzone({
           overlap: 0.75,
-          ondragenter: function (event) {
+          ondragenter: function(event) {
             toggleDragTarget(true, event);
           },
-          ondragleave: function (event) {
+          ondragleave: function(event) {
             toggleDragTarget(false, event);
           },
-          ondropdeactivate: function (event) {
+          ondropdeactivate: function(event) {
             toggleDragTarget(false, event);
           },
-          ondrop: function (event) {
+          ondrop: function(event) {
             var draggable = event.draggable;
-            if(draggable.data) {
-              var data = draggable.data;
-              //TODO: Call IDropZone drop
+            if (draggable.data && target.canDrop(draggable.data)) {
+              target.doDrop(draggable.data);
             }
           }
         });
       var baseDropChecker = interactable.dropCheck;
-      interactable.dropChecker(function (event, target, dragElement, rect) {
-        var overlaps = baseDropChecker.call(interactable, event, target, dragElement, rect);
-        if (overlaps) {
-          //TODO: Call IDropZone canDrop
-          console.log("overlap");
-        }
-        return overlaps;
+      interactable.dropChecker(function(event, interactableTarget, dragElement, rect) {
+        var overlaps = baseDropChecker.call(interactable, event, interactableTarget, dragElement, rect);
+        return overlaps && target.canDrop(interactableTarget.data);
       });
     }
   };
