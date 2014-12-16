efe8792a25c6806042144496adf79742d3cf9cb4
Ulrik Långström <odalrick@gmail.com>
2014-12-16 09:22:23 +0100

    Optimize Character list facet

--- js/client/facet/z.client.facet.CharacterListFacet.js
+++ js/client/facet/z.client.facet.CharacterListFacet.js
@@ -2,6 +2,7 @@ goog.provide('z.client.facet.CharacterListFacet');
 
 goog.require('z.client.facet.Facet');
 goog.require('z.client.facet.CharacterFacet');
+goog.require('z.common.entities.Character');
 goog.require('z.common.events');
 goog.require('z.common');
 goog.require('z.client');
@@ -16,7 +17,7 @@ goog.require('goog.array');
  * @constructor
  * @implements mugd.injector.IInjectable
  */
-z.client.facet.CharacterListFacet = function (services) {
+z.client.facet.CharacterListFacet = function(services) {
   goog.base(this);
 
   this._repo = /** @type {!z.common.EntityRepository}*/ services.get(z.common.Resources.REPOSITORY);
@@ -33,13 +34,13 @@ goog.inherits(z.client.facet.CharacterListFacet, z.client.facet.Facet);
 /**
  * @param {goog.events.EventTarget} parent
  */
-z.client.facet.CharacterListFacet.prototype.setParentEventTarget = function (parent) {
+z.client.facet.CharacterListFacet.prototype.setParentEventTarget = function(parent) {
   goog.base(this, 'setParentEventTarget', parent);
   this.eventHandler.listen(parent, z.common.events.EventType.ENTITY_CREATED, this.doEntityCreated);
   this.eventHandler.listen(parent, z.common.events.EventType.ENTITY_MODIFIED, this.doEntityModified);
 };
 
-z.client.facet.CharacterListFacet.prototype._getCharacterList = function () {
+z.client.facet.CharacterListFacet.prototype._getCharacterList = function() {
   var entityQuery = this._entityQuery();
   entityQuery.category = z.common.rulebook.category.CHARACTER_TYPE;
   var entities = this._repo.filter(entityQuery.match.bind(entityQuery));
@@ -55,23 +56,26 @@ z.client.facet.CharacterListFacet.prototype._getCharacterList = function () {
 /**
  * @param {!z.common.events.EntityCreated} e
  */
-z.client.facet.CharacterListFacet.prototype.doEntityCreated = function (e) {
-  /**
-   * @type {!z.common.entities.Entity}
-   */
-  var entity = e.entity;
-  this['characters'](this._getCharacterList());
+z.client.facet.CharacterListFacet.prototype.doEntityCreated = function(e) {
+  if (e.entity instanceof z.common.entities.Character) {
+    /**
+     * @type {!z.common.entities.Entity}
+     */
+    var entity = e.entity;
+    this['characters'](this._getCharacterList());
+  }
 };
 
 /**
  * @param {!z.common.events.EntityModified} e
  */
-z.client.facet.CharacterListFacet.prototype.doEntityModified = function (e) {
+z.client.facet.CharacterListFacet.prototype.doEntityModified = function(e) {
   /**
    * @type {!z.common.entities.Entity}
    */
   var entity = e.entity;
-  this['characters'](this._getCharacterList());
-
+  if (entity instanceof z.common.entities.Character) {
+    this['characters'](this._getCharacterList());
+  }
 };
 
